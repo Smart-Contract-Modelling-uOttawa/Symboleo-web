effe5151e0efc0925bfd92e9f0f9d856992ce467
Durga Sivakumar <41239586+durga256@users.noreply.github.com>
2023-10-13 10:16:42 -0400

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,7 +9,9 @@ const defaultCode = ``;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Fragment>
 <h2>Symboleo Web</h2>
-<a href='https://github.com/Smart-Contract-Modelling-uOttawa/Symboleo-IDE/tree/master'>Documentation</a>
+<pre>
+<a href='https://github.com/Smart-Contract-Modelling-uOttawa/Symboleo-IDE/tree/master'>Documentation</a>         <a href='https://github.com/Smart-Contract-Modelling-uOttawa/Symboleo-web'>SourceCode</a>
+</pre>
 <ReactMonacoEditor
     defaultCode={defaultCode}
     hostname={'localhost'}
